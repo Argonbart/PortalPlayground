3f96d627d63ad0e2905e7576aad6112e66a2b997
Alex <104043084+Argonbart@users.noreply.github.com>
2025-10-02 12:15:40 +0200

    Change projection to SCREEN_UV

--- PortalPlayground/portal.gd
+++ PortalPlayground/portal.gd
@@ -2,23 +2,53 @@ class_name Portal
 extends MeshInstance3D
 
 
+const PORTAL_SHADER = preload("uid://bnqa1pcv8pp71")
+const UV_DEBUGGING = preload("uid://dbgf723wn4bda")
+
+
 @export var player_cam: Camera3D
 @export var target_portal: Portal
 
+var mat
+
 
 func _ready() -> void:
-	set_portal_to_viewport(self, target_portal.get_child(0))
+	set_shader_texture(target_portal.get_child(0))
 
 
 func _process(_delta: float) -> void:
-	get_child(0).get_child(0).transform = self.global_transform * target_portal.global_transform.affine_inverse() * player_cam.global_transform
+	
+	var camera: Camera3D = get_child(0).get_child(0)
+	
+	# update camera transform
+	camera.transform = self.global_transform * target_portal.global_transform.affine_inverse() * player_cam.global_transform
+	
+	# update uvs
+	var corner_a: Vector3 = self.global_position + Vector3(-3.0, -2.0, 0.0)
+	var corner_b: Vector3 = self.global_position + Vector3(3.0, -2.0, 0.0)
+	var corner_c: Vector3 = self.global_position + Vector3(3.0, 2.0, 0.0)
+	var corner_d: Vector3 = self.global_position + Vector3(-3.0, 2.0, 0.0)
+	var corner_a_cam: Vector2 = camera.unproject_position(corner_a)
+	var corner_b_cam: Vector2 = camera.unproject_position(corner_b)
+	var corner_c_cam: Vector2 = camera.unproject_position(corner_c)
+	var corner_d_cam: Vector2 = camera.unproject_position(corner_d)
+	var corner_a_cam_adjusted_uv: Vector2 = corner_a_cam / camera.get_viewport().get_visible_rect().size
+	var corner_b_cam_adjusted_uv: Vector2 = corner_b_cam / camera.get_viewport().get_visible_rect().size
+	var corner_c_cam_adjusted_uv: Vector2 = corner_c_cam / camera.get_viewport().get_visible_rect().size
+	var corner_d_cam_adjusted_uv: Vector2 = corner_d_cam / camera.get_viewport().get_visible_rect().size
+	target_portal.mat.set_shader_parameter("c1", corner_a_cam_adjusted_uv)
+	target_portal.mat.set_shader_parameter("c2", corner_b_cam_adjusted_uv)
+	target_portal.mat.set_shader_parameter("c3", corner_c_cam_adjusted_uv)
+	target_portal.mat.set_shader_parameter("c4", corner_d_cam_adjusted_uv)
 
 
-func set_portal_to_viewport(portal: Portal, viewport: SubViewport):
-	var mat = portal.get_surface_override_material(0)
-	if mat == null:
-		mat = StandardMaterial3D.new()
-		portal.set_surface_override_material(0, mat)
-	mat.albedo_texture = viewport.get_texture()
-	mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	mat.cull_mode = BaseMaterial3D.CULL_DISABLED
+func set_shader_texture(viewport: SubViewport):
+	mat = get_surface_override_material(0)
+	
+	if mat == null or not (mat is ShaderMaterial):
+		mat = ShaderMaterial.new()
+		mat.shader = UV_DEBUGGING
+		set_surface_override_material(0, mat)
+	
+	# Now mat is guaranteed to be a ShaderMaterial
+	mat.set_shader_parameter("portal_tex", viewport.get_texture())
